d5a213545c60d4eb87af959010a3743666801ae4
mana95 <manojdharmarthne@gmail.com>
2021-07-31 17:53:35 +0530

    Chang

--- frontend/src/app/UserDashBoard/user-dashboard/SubComponents/Schedules/view-schedules/send-progress/send-progress.component.ts
+++ frontend/src/app/UserDashBoard/user-dashboard/SubComponents/Schedules/view-schedules/send-progress/send-progress.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit} from '@angular/core';
+esimport {Component, Inject, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {ScheduleService} from "../../../../../../service/schedule.service";
@@ -92,7 +92,7 @@ export class SendProgressComponent implements OnInit {
         if(!_findDup){
           controlName.push(
             this.formBuilder.group(
-             { par }   
+             { par }
             )
           )
         }
